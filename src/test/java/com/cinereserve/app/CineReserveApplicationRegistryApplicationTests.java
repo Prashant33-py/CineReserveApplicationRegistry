870a4aa5705b2550722f8192ff6b2c27e8e767c7
Prashant33-py <pvasala786@gmail.com>
2025-08-05 00:52:06 +0530

    Initial commit

---
+++ src/test/java/com/cinereserve/app/CineReserveApplicationRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinereserve.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineReserveApplicationRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
